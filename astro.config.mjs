e9f49d59106cf491a7b625a69017f00b15996de5
MD Razikul Islam Joni <razikuljoni@gmail.com>
2026-09-22 22:22:41 +0600

    fix(astro): set output static for hybrid pre-rendering with vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://razikuljoni.xyz',
-  output: 'server',
+  output: 'static',
   adapter: vercel(),
   integrations: [
     mdx({
